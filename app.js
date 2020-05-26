57f1be572750973675283005154548acbe10d11c
Trent Deckard <trent@Trents-MacBook-Pro.local>
2020-05-25 20:20:29 -0600

    Changes for Heroku

--- app.js
+++ app.js
@@ -169,6 +169,9 @@ app.delete('/list/deleteitem', function(req, res){
   }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+// Allow Heroku to Listen for Correct Port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
